1cb7984ce0f72b32fbed849eb683ea355c26f856
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-01-28 00:01:42 -0300

    se conecta con BBDD

--- src/db.js
+++ src/db.js
@@ -1,1 +1,14 @@
 //config de base de datos 
+
+import mongoose from "mongoose";
+
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://root:admin123@mern-stack.7wrii3j.mongodb.net/?retryWrites=true&w=majority");
+
+        console.log(">>> DB is connected");
+    } catch (error) {
+        console.error(error);
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 //arranca la aplicacion 
+
 import app from './app.js'
 
+import {connectDB} from './db.js'
+
+connectDB() //inicia la coneccion a la base de datos
 app.listen(3000)
 console.log("Server on port", 3000)
