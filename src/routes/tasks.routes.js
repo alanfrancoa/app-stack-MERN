18f543c4520fe50df92c1521ea98ae36cdd03852
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-03-03 02:59:06 -0300

    Se realiza CRUD de tareas, proximo validaciones.

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import {authRequired} from '../middlewares/validateToken.js'
+import { authRequired } from '../middlewares/validateToken.js'
+import {
+    getTasks,
+    getTask,
+    updateTask,
+    deleteTask,
+    createTask
+} from '../controllers/tasks.controller.js'
 
 const router = Router()
+//CRUD
+router.get('/tasks', authRequired, getTasks) //ver todos
+router.get('/tasks/:id', authRequired, getTask)//ver uno solo por ID
 
-router.get('/tasks',authRequired, (req, res) => res.send("tasks"))
+router.post('/tasks', authRequired, createTask)//subir una tarea
+router.delete('/tasks/:id', authRequired, deleteTask)//eliminar una tarea por ID
+router.put('/tasks/:id', authRequired, updateTask) //actualizar uno solo por ID
+
 
 export default router
