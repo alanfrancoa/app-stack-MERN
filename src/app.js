9bcfa85a70435cbe4e9f598bda624269cddd1e4a
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-03-03 01:43:11 -0300

    Se agrega middleware para ingresar como ruta logueada en, /tasks

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 //config de express
 import express from "express"
 import morgan from "morgan"
+
 import authRoutes from './routes/auth.routes.js'
+import taskRoutes from './routes/tasks.routes.js'
 import cookieParser from "cookie-parser"
 
 const app = express()
@@ -11,5 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api',authRoutes)
+app.use('/api',taskRoutes)
+
 
 export default app
