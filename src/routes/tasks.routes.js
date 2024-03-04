d9ff89702f9dfe47821054106e2f7ecd7397bae8
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-03-04 12:15:53 -0300

    Se hace validacion de tareas.Proximo, empezar el front.

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,13 +7,16 @@ import {
     deleteTask,
     createTask
 } from '../controllers/tasks.controller.js'
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
+
 
 const router = Router()
 //CRUD
 router.get('/tasks', authRequired, getTasks) //ver todos
 router.get('/tasks/:id', authRequired, getTask)//ver uno solo por ID
 
-router.post('/tasks', authRequired, createTask)//subir una tarea
+router.post('/tasks', authRequired, validateSchema(createTaskSchema), createTask)//subir una tarea, primero verifica si esta autorizado, luego verifica la info enviada, si esta todo OK, se crea la tarea.  
 router.delete('/tasks/:id', authRequired, deleteTask)//eliminar una tarea por ID
 router.put('/tasks/:id', authRequired, updateTask) //actualizar uno solo por ID
 
