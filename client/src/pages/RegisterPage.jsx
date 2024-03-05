88277289788fd8478d5918a5147a80056f6d632a
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-03-05 17:57:30 -0300

    registro de usuario realizado desde front.

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { registerRequest } from '../api/auth'
+
+const RegisterPage = () => {
+    const { register, handleSubmit } = useForm()
+
+    return (
+        <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
+            <form onSubmit={handleSubmit(async (values) => {
+                console.log(values)
+                const res = await registerRequest(values);
+                console.log(res)
+            })}>
+                <input type="text" {...register('username', { required: true })}
+                    className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+                    placeholder='Username'
+                />
+                <input type="email"{...register('email', { required: true })}
+                    className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+                    placeholder='Email'
+                />
+                <input type="password" {...register('password', { required: true })}
+                    className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+                    placeholder='password'
+                />
+                <button type='submit'>Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterPage
--- src/app.js
+++ src/app.js
@@ -5,15 +5,18 @@ import morgan from "morgan"
 import authRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 import cookieParser from "cookie-parser"
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors());
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api',authRoutes)
-app.use('/api',taskRoutes)
+app.use('/api', authRoutes)
+app.use('/api', taskRoutes)
 
 
 export default app
