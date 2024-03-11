0fe02e284bc14e82b12483b6cde866d09f001c7b
Alan Franco Alvarez <alvarezalanfranco@gmail.com>
2024-03-11 19:05:19 -0300

    Se muestran mensajes de error, en el login, traidos desde el back y luego del registro se redirecciona a /tasks.

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -13,16 +13,26 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [errors, setErrors] = useState([])
 
     const signup = async (user) => {
-        const res = await registerRequest(user);
-        console.log(res.data)
-        setUser(res.data)
+        try{
+            const res = await registerRequest(user);
+            console.log(res.data)
+            setUser(res.data)
+            setIsAuthenticated(true)
+        } catch (error) {
+            console.log(error.response)
+            setErrors(error.response.data)
+        }
     }
     return (
         <AuthContext.Provider value={{
             signup,
             user,
+            isAuthenticated,
+            errors,
         }}>
             {children}
         </AuthContext.Provider>
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,6 +6,10 @@ export const register = async (req, res) => {
     const { email, password, username } = req.body
 
     try {
+        const userFound  = await User.findOne({email})
+        if (userFound){
+            return res.status(400).json(["The email already exists."])
+        }
         const passwordHash = await bcrypt.hash(password, 10)
 
         const newUser = new User({
@@ -13,15 +17,15 @@ export const register = async (req, res) => {
             email,
             password: passwordHash,
         })
-        const userFound = await newUser.save()
-        const token = await createAccessToken({ id: userFound._id })
+        const userSaved = await newUser.save()
+        const token = await createAccessToken({ id: userSaved._id })
         res.cookie('token', token)
         res.json({
-            id: userFound._id,
-            username: userFound.username,
-            email: userFound.email,
-            createdAt: userFound.createdAt,
-            updatedAt: userFound.updatedAt,
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
         })
     } catch (error) {
         res.status(500).json({ message: error.message })
@@ -32,7 +36,7 @@ export const login = async (req, res) => {
     const { email, password } = req.body
 
     try {
-
+        
         const userFound = await User.findOne({ email })
         if (!userFound) return res.status(400).json({ message: "User not found" })
 
